8fdebab64240b3bd024f8805797a78882ba6d6af
Louis Opter <kalessin@kalessin.fr>
2015-09-18 22:10:07 -0700

    Actually fix set_waveform on big endian architectures
    http://gph.is/1tvJ2WD
    
    And test it this time.

---
+++ tests/lifx/wire_proto/test_wire_proto_float_endian_conversion.c
@@ -0,0 +1,15 @@
+#include <sys/tree.h>
+
+#include <endian.h>
+#include <stdint.h>
+
+#include "lifx/wire_proto.h"
+
+int
+main(void)
+{
+    union u { float f; uint32_t i; };
+    union u value = { .i = 0x11223344 };
+    union u new_value = { .f = lgtd_lifx_wire_htolefloat(value.f) };
+    return new_value.i != 0x44332211;
+}

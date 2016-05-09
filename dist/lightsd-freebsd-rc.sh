2f7018d9f77c35d3167edc8d48b1dbaf57ff3144
Charlie Root <root@hunters6.plus.com>
2016-05-09 11:31:39 +0100

    Add freebsd rc script

---
+++ dist/lightsd-freebsd-rc.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+#
+# $FreeBSD: head/net/lightsg/files/lightsd.in 367807 2014-09-10 09:36:24Z ehaupt $
+#
+
+# PROVIDE: lightsd
+# REQUIRE: LOGIN
+# BEFORE:  securelevel
+# KEYWORD: shutdown
+
+# Add the following lines to /etc/rc.conf to enable `lightsd':
+#
+# lightsd_enable="YES"
+# lightsd_flags="<set as needed>"
+#
+# See lightsg(1) for lightsd_flags
+#
+
+. /etc/rc.subr
+
+name="lightsd"
+rcvar=lightsd_enable
+
+command="/usr/sbin/daemon"
+start_precmd="lightsd_precmd"
+pidfile="/var/run/lightsd/$name.pid"
+
+# read configuration and set defaults
+load_rc_config "$name"
+: ${lightsd_enable="NO"}
+: ${lightsd_user:="lightsd"}
+: ${lightsd_group:="lightsd"}
+: ${lightsd_listen:="localhost:23456"}
+: ${lightsd_log:="info"}
+
+
+command_args="-P $pidfile -p $pidfile.cl -r /usr/local/bin/$name -l ${lightsd_listen} -v $lightsd_log -S 2>&1  "
+#command_args="-l ${lightsd_listen} -v debug "
+
+lightsd_precmd()
+{
+  # not a lot
+}
+
+run_rc_command "$1"
